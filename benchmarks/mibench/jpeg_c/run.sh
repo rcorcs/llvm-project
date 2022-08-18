7dbbaf79e80aa3f81336ff06220413a91b31add6
Pavlos Petoumenos <ppetoumenos@gmail.com>
2022-08-18 11:08:12 +0000

    Default MiBench inputs for all benchmarks

--- benchmarks/mibench/jpeg_c/run.sh
+++ benchmarks/mibench/jpeg_c/run.sh
@@ -1,5 +1,5 @@
 DIR=$(dirname $0)
 BENCH=jpeg_c
 FILENAME=$(basename $1)
-/usr/bin/time -f "${BENCH},${FILENAME},%E" sh -c "$1 -outfile /tmp/${BENCH}.${FILENAME}.out ${DIR}/data/data.ppm 1>/dev/null 2>&1"
+/usr/bin/time -f "${BENCH},${FILENAME},%E" sh -c "$1 -dct int -progressive -opt -outfile /tmp/${BENCH}.${FILENAME}.out ${DIR}/data/data.ppm 1>/dev/null 2>&1"
 
